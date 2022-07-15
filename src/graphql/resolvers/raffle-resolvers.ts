5ab37f0196d497246ca512613183f271cefe15b8
lark <smilews@mail.ru>
2022-07-15 07:40:45 +0300

    Add raffleId to vkEvent in GQL

--- src/graphql/resolvers/raffle-resolvers.ts
+++ src/graphql/resolvers/raffle-resolvers.ts
@@ -1,22 +1,8 @@
 import { PrismaClient } from '../../prisma/generated';
-import { IsRaffle } from '../generated/generated';
 
 const db = new PrismaClient();
 
 export const resolvers = {
-    Query: {
-        getRaffleId: async (obj: any, args: { eventId: number }): Promise<IsRaffle> => {
-            const raffle = await db.raffle.findUnique({
-                where: {
-                    eventId: args.eventId
-                }
-            });
-            if(raffle)
-                return { raffle: true, raffleId: raffle.id };
-            else
-                return { raffle: false };
-        },    
-    },
     Mutation: {
         takePart: async (_: any, input: { userId: number, raffleId: number }): Promise<string> => {
             const existingRaffleUser = await db.raffleUser.findUnique({
